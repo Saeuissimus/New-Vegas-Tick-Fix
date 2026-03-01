3ac3c2e7d8bf61fa4e6daf61b6b70dc6338bf322
Saeuissimus <nalus_saeuus@protonmail.com>
2026-03-01 01:40:29 -0300

    simplifies locking for some hooks

--- nvtf/internal/Common/CommonUtils.hpp
+++ nvtf/internal/Common/CommonUtils.hpp
@@ -1,15 +1,80 @@
 #pragma once
 
+#include "internal/Common/CommonUtils.hpp"
+#include <vector>
+#include "nvse/PluginAPI.h"
+#include <algorithm>
+
+class SRWSharedLock {
+public:
+	SRWSharedLock(PSRWLOCK apLock) : pLock(apLock) { AcquireSRWLockShared(pLock); }
+	SRWSharedLock(SRWLOCK& arLock) : pLock(&arLock) { AcquireSRWLockShared(pLock); }
+	~SRWSharedLock() { ReleaseSRWLockShared(pLock); }
+private:
+	PSRWLOCK pLock;
+};
+
+class SRWUniqueLock {
+public:
+	SRWUniqueLock(PSRWLOCK apLock) : pLock(apLock) { AcquireSRWLockExclusive(pLock); }
+	SRWUniqueLock(SRWLOCK& arLock) : pLock(&arLock) { AcquireSRWLockExclusive(pLock); }
+	~SRWUniqueLock() { ReleaseSRWLockExclusive(pLock); }
+private:
+	PSRWLOCK pLock;
+};
+
 namespace CommonUtils
 {
-	class UtilsInterface
+	class UtilsManager
 	{
-	public:
-		virtual ~UtilsInterface() {}
-		virtual bool RegisterMainLoopFunction(bool(__cdecl *pToRegister)()) = 0;
-		virtual bool UnregisterMainLoopFunction(bool(__cdecl *pToUnregister)()) = 0;
+	private:
+		std::vector<bool(__cdecl*)()> m_Callbacks;
+		SRWLOCK srwlock = SRWLOCK_INIT;
 
+	public:
+		bool RegisterMainLoopFunction(bool (__cdecl *pToRegister)()) {
+			SRWUniqueLock lock(srwlock);
+			auto it = std::find(m_Callbacks.begin(), m_Callbacks.end(), pToRegister);
+			if (it == m_Callbacks.end())
+				m_Callbacks.push_back(pToRegister);
+			return true;
+		};
+		bool UnregisterMainLoopFunction(bool (__cdecl *pToRegister)()) {
+			SRWUniqueLock lock(srwlock);
+			m_Callbacks.erase(std::remove(m_Callbacks.begin(), m_Callbacks.end(), pToRegister), m_Callbacks.end());
+			return true;
+		};
+
+		std::vector<bool(__cdecl*)()> copyCallbacks() {
+			SRWSharedLock lock(srwlock);
+			return m_Callbacks;
+		}
 	};
-	UtilsInterface* GetUtilsManager();
-	void NVSEMessageHandler(void* msg);
+
+	inline UtilsManager* GetUtilsManager()
+	{
+		static UtilsManager g_Utils;
+		return &g_Utils;
+	}
+
+	inline void NVSEMessageHandler(void* receivedMsg)
+	{
+		auto msg = static_cast<NVSEMessagingInterface::Message*>(receivedMsg);
+		switch (msg->type)
+		{
+		case NVSEMessagingInterface::kMessage_MainGameLoop:
+			{
+				auto lUtilsMan = GetUtilsManager();
+				auto copiedCallbacks = lUtilsMan->copyCallbacks();
+
+				for (auto callback : copiedCallbacks)
+				{
+					callback();
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
